0305778a2b836f83eefc126a84da55258f877ea1
Zeynep Akkalyoncu <zeynep.akkalyoncu@metu.edu.tr>
2017-07-08 16:51:47 +0300

    Initial Commit

---
+++ Review/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Review
+//
+//  Created by Zeynep Akkalyoncu on 08/07/2017.
+//  Copyright © 2017 Zeynep Akkalyoncu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
